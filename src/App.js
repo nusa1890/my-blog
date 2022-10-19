bf3c5ccec8c5c26954f55c8182ff285a45495a71
nusa1890 <imadenusayudiskara@gmail.com>
2022-10-19 15:40:34 +0800

    Working website button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import TopBar from "./components/topbar/TopBar";
 import Register from "./pages/register/Register";
-/*
 import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import Home from "./pages/home/Home.jsx"
 import Single from "./pages/single/Single.jsx"
 import Write from "./pages/write/Write";
-*/
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const user=false;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Register />
-    </>
+      <Routes>
+        <Route path='/' element={user ? <Home /> : <Register/>} />
+        <Route path='/login' element={user ? <Home /> : <Login />} />
+        <Route path='/register' element={user ? <Home/> : <Register />} />
+        <Route path='/settings' element={user ? <Settings /> : <Register/>} />
+        <Route path='/write' element={user ? <Write /> : <Register/>} />
+        <Route path='/post/:postId' element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,7 @@
 import "./topbar.css"
-
+import {Link} from "react-router-dom";
 export default function TopBar() {
+  const user = false;
   return (
     <div className='top'>
       <div className="topLeft">
@@ -11,20 +12,33 @@ export default function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem"><Link className="link" to="/"> HOME</Link></li>
+          <li className="topListItem"><Link className="link" to="/about"> ABOUT</Link></li>
+          <li className="topListItem"><Link className="link" to="/contact"> CONTACT</Link></li>
+          <li className="topListItem"><Link className="link" to="/write"> WRITE</Link></li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
         </ul>
       </div>
         
       <div className="topRight">
-        <img
-        className="topImage"
-        src="https://img.freepik.com/premium-photo/profile-portrait-cheerful-nice-adviser-guy-direct-thumb-empty-space-yellow-background_525549-6705.jpg?w=740"
-        alt=""
-        />
+        {
+          user ? (
+            <img
+              className="topImage"
+              src="https://img.freepik.com/premium-photo/profile-portrait-cheerful-nice-adviser-guy-direct-thumb-empty-space-yellow-background_525549-6705.jpg?w=740"
+              alt=""
+            />
+          ):(
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">LOGIN</Link>
+              </li> 
+              <li className="topListItem">
+                <Link className="link" to="/register">REGISTER</Link>
+              </li>
+            </ul>
+          )
+        }
         <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
